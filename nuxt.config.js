e3cb7155365e5668a7c9d821a23520b424426963
JackDownload <kismocsy@gmail.com>
2021-08-23 08:08:07 +0200

    feltölt

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,10 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/gtm',
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
@@ -62,6 +66,15 @@ export default {
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
+  sitemap: {
+    hostname: 'https://josies-recipes.netlify.app/',
+    gzip: true,
+    routes: async () => {
+      const { $content } = require('@nuxt/content')
+      const articles = await $content('articles').only(['path']).fetch()
+      return articles.map((p) => p.path)
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
